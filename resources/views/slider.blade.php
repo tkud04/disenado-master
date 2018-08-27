8ff3d49aa2a12dcfbd774327df174f9004b4c6bb
ANonymous <postmaster.richmama@gmail.com>
2018-08-27 14:42:42 +0100

    changes to slider blade

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -11,7 +11,12 @@
 								{!! csrf_field() !!}
 								<div class="select-style">
 									<select class="select-optn ">
-										<option class="first-option">all category</option>
+										<option class="first-option">all</option>
+										<option class="">laptops</option>
+										<option class="">fashion</option>
+										<option class="">tablets</option>
+										<option class="">electronics</option>
+										<option class="">fragrances</option>
 									</select>
 								</div>
 								<input class="input-text-box" name="term" spellcheck="false" value="" placeholder="Search product...">
