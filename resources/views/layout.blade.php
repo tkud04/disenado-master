40046f7f4a25ead803791e57c870a275c4226a94
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-09-06 15:51:14 +0100

    Moved notifs to layout blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,21 @@
                     @if (count($errors) > 0)
                           @include('input-errors', ["errors" => $errors])
                      @endif   
-                     
+                    
+@if(Session::has('cart-status') && Session::get('cart-status') == "new-user")
+			 <center><div class="alert alert-success">Signup successful! <a href="#" class="legbe">Click here</a> to login and verify your account.</div></center>
+			@endif
+			
+    @if(Session::has('cart-status') && Session::get('cart-status') == "new-user")
+			 <center><div class="alert alert-success">You must <a href="#" class="legbe">login</a> or <a href="#" class="legbe2">signup</a> to continue.</div></center>
+			@endif
+	    @if(Session::has('add-to-cart-status') && Session::get('add-to-cart-status') == "success")
+			 <center><div class="alert alert-success">Added to cart!</div></center>
+
+     	@elseif(Session::has('remove-from-cart-status') && Session::get('remove-from-cart-status') == "success")
+			 <center><div class="alert alert-success">Removed from cart!</div></center>
+	    @endif
+	
 @yield('content') 
 
 @include("footer")
