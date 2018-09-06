03855fc728012f111d0ad2e680f648d062ff090e
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-09-06 15:36:31 +0100

    Moved notifs to index

--- resources/views/shop-content.blade.php
+++ resources/views/shop-content.blade.php
@@ -3,15 +3,6 @@
 		<div class="main-content">
 			<div class="container">
 				<div class="row">
-	    @if(Session::has('cart-status') && Session::get('cart-status') == "new-user")
-			 <center><div class="alert alert-success">You must <a href="#" class="legbe">login</a> or <a href="#" class="legbe2">signup</a> to continue.</div></center>
-			@endif
-	    @if(Session::has('add-to-cart-status') && Session::get('add-to-cart-status') == "success")
-			 <center><div class="alert alert-success">Added to cart!</div></center>
-
-     	@elseif(Session::has('remove-from-cart-status') && Session::get('remove-from-cart-status') == "success")
-			 <center><div class="alert alert-success">Removed from cart!</div></center>
-	    @endif
 					<div class="col-md-3 col-xs-12" >
 						<div class="widget">
 							<h3>Categories</h3>
