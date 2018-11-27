a4da0876285aeda5ab0adf4727751823c3dabc38
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-11-27 16:30:22 +0100

    Upload msg blade

---
+++ resources/views/emails/msg.blade.php
@@ -0,0 +1,11 @@
+<center><h3 style="margin: 1.2em; color: #00FF61;">You have a new message!</h3></center>
+
+<p>Name: <span style="font-weight:bold; color: #0070FF;">{{$name}}</span></p>
+
+<p>Email: <span style="font-weight:bold; color: #0070FF;">{{$email}}</span></p>
+
+<p>Phone: <span style="font-weight:bold; color: #0070FF;">{{$phone}}</span></p>
+
+<p>Message: <span style="font-weight:bold; color: #0070FF;">{{$msg}}-</span></p>
+
+
