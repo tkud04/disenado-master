cd27b197b6ee610be2a84a3090e47fb46a387492
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-04-25 13:52:46 +0100

    Add files via upload

---
+++ resources/views/umbrella.blade.php
@@ -0,0 +1,39 @@
+@include("head")
+
+		<!-- Main content  -->
+		<div class="contact-body">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="heading-title">
+							<h4>WELCOME TO DISENADO NG</h4>
+						</div> 
+					</div>
+					<div class="about-area">
+						<div class="col-md-6 col-sm-6 col-xs-12">
+							<div class="content">
+								<div class="card bg-dark text-white">
+  <img src="img/it.png" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <a class="btn btn-primary btn-lg">Go to IT Services</p>
+  </div>
+</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6 col-xs-12">
+							<div class="content">
+								<div class="card bg-dark text-white">
+  <img src="img/shop.png" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <a class="btn btn-primary btn-lg">Go to Online Shop</p>
+  </div>
+</div>
+							</div>
+						</div>
+					</div>
+				</div>
+		  </div>
+      </div>
+		<!-- Main content end -->
+
+@include("foot")
